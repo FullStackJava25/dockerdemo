179279e50ffdf31260c6a304275b1d4f09b6d6a7
fullstack <fullstacktraining25@gmail.com>
2025-06-14 23:16:36 +0530

    updated folder

--- src/main/java/com/example/DockerDemo/DockerDemoApplication.java
+++ src/main/java/com/example/DockerDemo/DockerDemoApplication.java
@@ -2,7 +2,6 @@ package com.example.DockerDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//springapp
 @SpringBootApplication
 public class DockerDemoApplication {
 
